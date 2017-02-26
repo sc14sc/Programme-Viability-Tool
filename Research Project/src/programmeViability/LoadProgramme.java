18b525b1fdfa51d9f2bb159aed2be0893ce67b68
Sitong Chen <sc14sc@leeds.ac.uk>
2017-02-26 13:46:49 +0000

    first commit

---
+++ Research Project/src/programmeViability/LoadProgramme.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package programmeViability;
+
+import javax.swing.JFrame;
+
+/**
+ * @author sitongchen
+ *
+ */
+public class LoadProgramme {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LoadProgramme loadProgramme = new LoadProgramme();
+
+	}
+	public LoadProgramme() {
+		JFrame screen1 = new JFrame("Programme Viability Tool");
+		
+		screen1.setSize(1000, 600);
+		screen1.setLocationRelativeTo(null);
+		screen1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		screen1.setVisible(true);
+	}
+
+}
