c0c5aaa3317dc51091de38dfe6c3db2a11b9695d
Sitong Chen <sc14sc@leeds.ac.uk>
2017-02-26 14:14:14 +0000

    Added panel and buttons

--- Research Project/src/programmeViability/LoadProgramme.java
+++ Research Project/src/programmeViability/LoadProgramme.java
@@ -3,7 +3,13 @@
  */
 package programmeViability;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * @author sitongchen
@@ -21,6 +27,37 @@ public class LoadProgramme {
 	}
 	public LoadProgramme() {
 		JFrame screen1 = new JFrame("Programme Viability Tool");
+		
+		JPanel basic = new JPanel();
+		basic.setMaximumSize(basic.getPreferredSize());
+		basic.setLayout(new BoxLayout(basic, BoxLayout.Y_AXIS));
+		screen1.add(basic);
+		
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+		JPanel midPanel = new JPanel();
+		midPanel.setLayout(new BoxLayout(midPanel, BoxLayout.X_AXIS));
+		JPanel botPanel = new JPanel();
+		botPanel.setLayout(new BoxLayout(botPanel, BoxLayout.X_AXIS));
+		
+		JButton loadFileButton = new JButton("Load File");
+		JButton addModuleButton = new JButton("Add Module");
+		
+		JButton exitButton = new JButton("Exit");
+		exitButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent evebt){
+				System.exit(1);
+			}
+		});
+		
+		topPanel.add(loadFileButton);
+		topPanel.add(addModuleButton);
+		botPanel.add(exitButton);
+		
+		basic.add(topPanel);
+		basic.add(midPanel);
+		basic.add(botPanel);
 		
 		screen1.setSize(1000, 600);
 		screen1.setLocationRelativeTo(null);
