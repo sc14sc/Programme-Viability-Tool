7e7fa40cce0ae43d7fef11ec11ccaaac5cd2c338
Sitong Chen <sc14sc@leeds.ac.uk>
2017-02-26 14:41:06 +0000

    Added Title

--- Research Project/src/programmeViability/LoadProgramme.java
+++ Research Project/src/programmeViability/LoadProgramme.java
@@ -3,12 +3,16 @@
  */
 package programmeViability;
 
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -34,7 +38,11 @@ public class LoadProgramme {
 		screen1.add(basic);
 		
 		JPanel topPanel = new JPanel();
-		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
+		JPanel titlePanel = new JPanel();
+		titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.X_AXIS));
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
 		JPanel midPanel = new JPanel();
 		midPanel.setLayout(new BoxLayout(midPanel, BoxLayout.X_AXIS));
 		JPanel botPanel = new JPanel();
@@ -51,8 +59,17 @@ public class LoadProgramme {
 			}
 		});
 		
-		topPanel.add(loadFileButton);
-		topPanel.add(addModuleButton);
+		JLabel programmeLabel = new JLabel("Please Load File");
+		programmeLabel.setHorizontalAlignment(JLabel.LEFT);
+		programmeLabel.setFont(new Font("Serif", Font.BOLD, 24));
+		
+		titlePanel.add(programmeLabel);
+		titlePanel.add(Box.createRigidArea(new Dimension(20,0)));
+		topPanel.add(titlePanel);
+		topPanel.add(buttonPanel);
+		buttonPanel.add(loadFileButton);
+		buttonPanel.add(Box.createRigidArea(new Dimension(500,0)));
+		buttonPanel.add(addModuleButton);
 		botPanel.add(exitButton);
 		
 		basic.add(topPanel);
