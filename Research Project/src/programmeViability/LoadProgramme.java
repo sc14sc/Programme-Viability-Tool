aeb7c27bec9d2848497a58e408a2b359420f74f1
Sitong Chen <sc14sc@leeds.ac.uk>
2017-02-26 17:11:16 +0000

    added method for action listener of loadFileButton

--- Research Project/src/programmeViability/LoadProgramme.java
+++ Research Project/src/programmeViability/LoadProgramme.java
@@ -7,10 +7,18 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -24,6 +32,9 @@ public class LoadProgramme {
 	/**
 	 * @param args
 	 */
+	
+	JLabel programmeLabel = new JLabel("Please Load File");
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LoadProgramme loadProgramme = new LoadProgramme();
@@ -49,17 +60,23 @@ public class LoadProgramme {
 		botPanel.setLayout(new BoxLayout(botPanel, BoxLayout.X_AXIS));
 		
 		JButton loadFileButton = new JButton("Load File");
+		loadFileButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent event){
+				loadFile();
+			}
+
+		});
 		JButton addModuleButton = new JButton("Add Module");
 		
 		JButton exitButton = new JButton("Exit");
 		exitButton.addActionListener(new ActionListener(){
 			@Override
-			public void actionPerformed(ActionEvent evebt){
+			public void actionPerformed(ActionEvent event){
 				System.exit(1);
 			}
 		});
 		
-		JLabel programmeLabel = new JLabel("Please Load File");
 		programmeLabel.setHorizontalAlignment(JLabel.LEFT);
 		programmeLabel.setFont(new Font("Serif", Font.BOLD, 24));
 		
@@ -80,6 +97,31 @@ public class LoadProgramme {
 		screen1.setLocationRelativeTo(null);
 		screen1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		screen1.setVisible(true);
+	}
+	protected void loadFile() {
+		// TODO Auto-generated method stub
+		JFileChooser fileChooser = new JFileChooser();
+		int returnValue = fileChooser.showOpenDialog(null);
+		if (returnValue == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = fileChooser.getSelectedFile();
+			programmeLabel.setText(selectedFile.getName());
+			try (
+					FileInputStream fis = new FileInputStream(selectedFile);
+					InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+					BufferedReader br = new BufferedReader(isr);
+					) {
+				String line = br.readLine();
+                String[] module = line.split(",");
+
+                System.out.println("Module code = " + module[5] + module[6]);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
